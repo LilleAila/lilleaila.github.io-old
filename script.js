6d95bd0d7fb2608aa07285f712f5cbdcda2c4094
LilleAila <olai.solsvik@gmail.com>
2022-03-14 18:47:15 +0000

    Rewrite all code

---
+++ script.js
@@ -0,0 +1,40 @@
+const username = "LilleAila";
+
+$.ajax({
+  dataType: "json",
+  type: "get",
+  url: `https://api.github.com/users/${username}/repos`,
+  success: (data) => {
+    // console.log(data);
+
+    for (let i = 0; i < data.length; i++) {
+      const current = data[i];
+
+      //   console.log(current);
+
+      const name = $("<a>")
+        .addClass("name")
+        .text(current.name)
+        .attr("href", current.html_url);
+      const language = $("<span>").addClass("language").text(current.language);
+
+      const element = $("<div>").addClass("repo").append(name).append(language);
+
+      $(".repos").append(element);
+    }
+  },
+  error: console.log,
+});
+
+$.ajax({
+  dataType: "json",
+  type: "get",
+  url: `https://api.github.com/users/${username}`,
+  success: (data) => {
+      $(".avatar").attr("src", data.avatar_url);
+      $(".username").text(data.name);
+      $(".bio").text(data.bio);
+      $(".reposCount").text(`Repos: ${data.public_repos}`);
+  },
+  error: console.log,
+});
